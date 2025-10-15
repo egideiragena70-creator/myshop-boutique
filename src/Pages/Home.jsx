9679f606fae1a0b0695a3587b3a96388636987d2
Egide Iragena <egideiragena70@gmail.com>
2025-10-16 04:44:22 +1200

    thank for commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center mt-10">
-      <h1 className="text-4xl font-bold mb-4">Welcome to MyShop Boutique!</h1>
+      <h1 className="text-4xl font-bold mb-4">EGIDE DEVELPER!</h1>
       <p className="text-gray-600">Find the best clothing and accessories here.</p>
     </div>
   );
